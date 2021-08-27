60f7e1cf0cf2919ade83cd850cae57f206c08117
Bram <gitkraken@bram.blmgroep.nl>
2021-08-28 01:50:42 +0200

    Prepare website for translations

---
+++ js/language-picker.js
@@ -0,0 +1,59 @@
+const languages = {
+//  'hr': 'Croatian',       // Not supported yet
+    'da': 'Dansk',          // Supports Danish
+    'de': 'Deutsch',        // Supports German
+    'et': 'Eesti',          // Supports Estonian
+    'en': 'English',        // Supports English
+    'es': 'Español',        // Supports Spanish
+    'fr': 'Français',       // Supports French
+//  'ga': 'Irish',          // Not supported yet
+    'it': 'Italiano',       // Supports Italian
+    'lv': 'Latviešu',       // Supports Latvian
+    'lt': 'Lietuvių kalba', // Supports Lithuanian
+    'hu': 'Magyar',         // Supports Hungarian
+//  'mt': 'Maltese',        // Not supported yet
+    'nl': 'Nederlands',     // Supports Dutch
+    'pl': 'Polski',         // Supports Polish
+    'pt': 'Portugês',       // Supports Portuguese
+    'ro': 'Românesc',       // Supports Romanian
+    'sl': 'Slovenski',      // Supports Slovenian
+    'sk': 'Slovenská',      // Supports Slovak
+    'fi': 'Suomalainen',    // Supports Finnish
+    'sv': 'Svenska',        // Supports Swedish
+    'cs': 'Česky',          // Supports Czech
+    'el': 'Ελληνική',       // Supports Greek
+    'bg': 'Български',      // Supports Bulgarian
+};
+
+let route = window.location.pathname;
+var currentLanguage = "en";
+
+// Determine current language
+for (lan in languages) {
+    if (route.startsWith("/" + lan + "/")) {
+        currentLanguage = route.slice(1, 3);
+        route = route.slice(3,);
+        break;
+    }
+}
+
+document.getElementById("language-picker").innerHTML = `
+<div>
+    <img src="icons/flags/${currentLanguage}.jpg" style="height: 30px;">
+</div>
+<div id="picker"></div>
+`;
+
+// Build language picker
+let languagePicker = "";
+
+for (lan in languages) {
+    languagePicker = languagePicker.concat(`
+        <a href="${lan}/${route}">
+            <img src="icons/flags/${lan}.jpg">
+            <span>${languages[lan]}</span>
+        </a>
+    `);
+}
+
+document.getElementById("picker").innerHTML = languagePicker;
--- js/preview-ideals.js
+++ js/preview-ideals.js
@@ -1,35 +1,139 @@
-var ideals = [
-    {
-        name: "Software Warranty",
-        description: "Software doesn't erode from natural causes, therefore deserves a longer life expectancy.",
-        image: "images/software-warranty.png",
-        page: "/software-warranty.html"
+var ideals = [{
+    "name": 'Software Warranty',
+    "description": {
+        "da": 'Software eroderer ikke af naturlige årsager og fortjener derfor en længere levetid.',
+        "de": 'Software erodiert nicht durch natürliche  Ursachen und verdient daher eine längere Lebenserwartung.',
+        "et": 'Tarkvara ei erodeeru looduslikel põhjustel, mistõttu väärib pikemat eluiga.',
+        "en": "Software doesn't erode from natural  causes, therefore deserves a longer life expectancy.",
+        "es": 'El software no se erosiona por causas naturales, por lo que merece una mayor esperanza de vida.',
+        "fr": "Les logiciels ne s'érodent pas par des causes naturelles, ils méritent donc une plus grande espérance de vie.",
+        "it": 'Il software non si erode per cause naturali, quindi merita una vita più lunga.',
+        "lv": 'Programmatūra nesabojājas dabisku iemeslu dēļ, tāpēc ir pelnījusi ilgāku kalpošanas laiku.',
+        "lt": 'Programinė įranga nesusidėvi dėl natūralių priežasčių, todėl nusipelno ilgesnio tarnavimo  laiko.',
+        "hu": 'A szoftver nem erodálódik természetes okokból, ezért hosszabb élettartamot érdemel.',
+        "nl": 'Software erodeert niet door natuurlijke oorzaken, daarom verdient het een langere levensverwachting.',
+        "pl": 'Oprogramowanie nie ulega erozji z przyczyn naturalnych, dlatego zasługuje na dłuższą żywotność.',
+        "pt": 'O software não sofre erosão por causas naturais, pelo que merece uma esperança de vida mais longa.',
+        "ro": 'Software-ul nu se erodează din cauze naturale, prin urmare merită o speranță de viață mai mare.',
+        "sl": 'Programska oprema se ne uničuje zaradi naravnih vzrokov, zato si zasluži daljšo življenjsko dobo.',
+        "sk": 'Softvér sa neznehodnocuje z prirodzených príčin, preto si zaslúži dlhšiu životnosť.',
+        "fi": 'Ohjelmistot eivät rapistu luonnollisista syistä, joten ne ansaitsevat pidemmän eliniän.',
+        "sv": 'Mjukvara eroderar inte av naturliga orsaker och förtjänar därför en längre livslängd.',
+        "cs": 'Software se nezničí z přirozených příčin, a proto si zaslouží delší životnost.',
+        "el": 'Το λογισμικό δεν  διαβρώνεται από φυσικά αίτια, επομένως αξίζει μεγαλύτερο προσδόκιμο ζωής.',
+        "bg": 'Софтуерът не ерозира по естествени причини, затова заслужава по-дълъг живот.'
     },
-    {
-        name: "Freedom of Installation",
-        description: "No one should be required or expected to run specific software on their electronic device.",
-        image: "images/installation-freedom.png",
-        page: "/freedom-of-installation.html"
+    "image": 'https://digital-justice.com/images/software-warranty.png',
+    "page": 'software-warranty.html'
+}, {
+    "name": 'Freedom of Installation',
+    "description": {
+        "da": 'Ingen bør være forpligtet til eller forventes at køre specifik software på deres elektroniske enhed.',
+        "de": 'Von niemandem sollte verlangt oder erwartet werden, dass er bestimmte Software auf seinem elektronischen Gerät ausführt.',
+        "et": 'Kelleltki ei tohiks nõuda ega oodata, et tema elektroonilises seadmes jookseks konkreetne tarkvara.',
+        "en": 'No one should be required or expected to run specific software on their electronic device.',
+        "es": 'No se debería exigir ni esperar que nadie ejecute un software específico en su dispositivo electrónico.',
+        "fr": "Personne ne devrait être obligé d'utiliser un logiciel spécifique sur son appareil électronique.",
+        "it": 'Nessuno dovrebbe essere obbligato o pretendere di eseguire un software specifico sul proprio dispositivo elettronico.',
+        "lv": 'Nevienam nevajadzētu prasīt vai sagaidīt, lai viņa elektroniskajā ierīcē tiktu instalēta īpaša programmatūra.',
+        "lt": 'Niekas neturėtų reikalauti ar tikėtis, kad jo elektroniniame įrenginyje būtų įdiegta tam tikra programinė įranga.',
+        "hu": 'Senkitől sem szabad megkövetelni vagy elvárni,  hogy bizonyos szoftvereket futtasson az elektronikus eszközén.',
+        "nl": 'Niemand zou verplicht  moeten worden om specifieke software op zijn elektronische apparaat te draaien.',
+        "pl": 'Nikt  nie powinien być zobowiązany lub oczekiwany do uruchamiania konkretnego oprogramowania na swoim urządzeniu elektronicznym.',
+        "pt": 'Ninguém deve ser obrigado ou esperado a executar software específico no seu dispositivo electrónico.',
+        "ro": 'Nimănui nu ar trebui să i se ceară sau să se aștepte să ruleze un anumit software pe dispozitivul său electronic.',
+        "sl": 'Od nikogar  se ne sme zahtevati ali pričakovati, da bo na svoji elektronski napravi uporabljal določeno programsko opremo.',
+        "sk": 'Od nikoho by sa nemalo vyžadovať ani očakávať, že na svojom elektronickom zariadení spustí špecifický softvér.',
+        "fi": 'Ketään ei pitäisi vaatia tai odottaa käyttämään tiettyjä ohjelmistoja sähköisessä laitteessaan.',
+        "sv": 'Ingen bör krävas eller förväntas köra en viss programvara på sin elektroniska enhet.',
+        "cs": 'Od nikoho by se nemělo vyžadovat ani očekávat, že na svém elektronickém zařízení spustí určitý software.',
+        "el": 'Κανείς δεν  πρέπει να υποχρεούται ή να αναμένεται να εκτελεί συγκεκριμένο λογισμικό στην ηλεκτρονική του συσκευή.',
+        "bg": 'Никой не трябва да бъде задължаван или да се очаква да стартира определен софтуер на своето електронно устройство.'
     },
-    {
-        name: "Software Autonomy",
-        description: "Walled gardens limit freedom. Anyone should have the right to reprogram their device.",
-        image: "images/software-autonomy.png",
-        page: "/software-autonomy.html"
+    "image": 'https://digital-justice.com/images/installation-freedom.png',
+    "page": 'freedom-of-installation.html'
+}, {
+    "name": 'Software Autonomy',
+    "description": {
+        "da": 'Muromkransede haver begrænser friheden. Enhver bør have ret til at omprogrammere sin enhed.',
+        "de": 'Walled Gardens schränken die Freiheit ein. Jeder sollte das Recht haben, sein Gerät umzuprogrammieren.',
+        "et": 'Müüriga piiratud aiad piiravad vabadust. Igaühel peaks olema õigus oma seadet ümber programmeerida.',
+        "en": 'Walled gardens limit freedom. Anyone should have the right to reprogram their device.',
+        "es": 'Los jardines amurallados limitan la libertad. Cualquiera debería tener derecho a reprogramar su dispositivo.',
+        "fr": 'Les jardins clos limitent la liberté. Tout le monde devrait avoir le droit de reprogrammer son appareil.',
+        "it": 'I giardini recintati limitano la libertà. Chiunque dovrebbe avere il diritto di riprogrammare il proprio dispositivo.',
+        "lv": 'Norobežoti dārzi ierobežo brīvību. Ikvienam jābūt tiesībām pārprogrammēt savu ierīci.',
+        "lt": 'Uždari sodai riboja laisvę. Kiekvienas turėtų turėti teisę perprogramuoti savo įrenginį.',
+        "hu": 'A fallal körülvett kertek korlátozzák a szabadságot. Bárkinek joga kell, hogy legyen átprogramozni a készülékét.',
+        "nl": 'Ommuurde tuinen beperken de vrijheid. Iedereen moet het recht hebben om zijn apparaat te herprogrammeren.',
+        "pl": 'Murowane ogrody ograniczają wolność. Każdy powinien mieć prawo do przeprogramowania swojego urządzenia.',
+        "pt": 'Jardins murados limitam a liberdade. Qualquer pessoa deve ter o direito  de reprogramar o seu dispositivo.',
+        "ro": 'Grădinile îngrădite limitează libertatea. Oricine ar trebui să aibă dreptul de a-și reprograma dispozitivul.',
+        "sl": 'Zidani vrtovi omejujejo svobodo. Vsakdo bi moral imeti pravico do reprogramiranja svoje naprave.',
+        "sk": 'Ohradené záhrady obmedzujú slobodu. Každý by mal mať právo preprogramovať svoje zariadenie.',
+        "fi": 'Muuratut puutarhat rajoittavat vapautta. Jokaisella pitäisi olla oikeus ohjelmoida laitteensa uudelleen.',
+        "sv": 'Murade trädgårdar begränsar friheten. Vem som helst bör ha rätt att programmera om sin enhet.',
+        "cs": 'Ohraničené zahrady omezují svobodu. Každý by měl mít právo přeprogramovat své zařízení.',
+        "el": 'Οι περιφραγμένοι κήποι περιορίζουν την ελευθερία. Ο καθένας θα πρέπει  να έχει το δικαίωμα να επαναπρογραμματίσει τη συσκευή του.',
+        "bg": 'Заградените градини ограничават свободата. Всеки трябва да има право да препрограмира устройството си.'
     },
-    {
-        name: "Abandonware Continuity",
-        description: "In contrast to Mickey Mouse, not being able to use software from a dead person may harm humans.",
-        image: "images/abandonware-continuity.png",
-        page: "/abandonware-continuity.html"
+    "image": 'https://digital-justice.com/images/software-autonomy.png',
+    "page": 'software-autonomy.html'
+}, {
+    "name": 'Abandonware Continuity',
+    "description": {
+        "da": 'I modsætning til Mickey Mouse kan det skade mennesker, hvis man ikke kan bruge software fra en død person.',
+        "de": 'Im Gegensatz zu Mickey Mouse kann es Menschen schaden, wenn sie die Software einer toten Person nicht nutzen können.',
+        "et": 'Erinevalt Miki Hiirest võib inimestele kahju teha see, et nad ei saa kasutada surnud inimese tarkvara.',
+        "en": 'In contrast to Mickey Mouse, not being able to use software from a dead person may harm humans.',
+        "es": 'A diferencia de Mickey Mouse, no poder utilizar el  software de un muerto puede perjudicar a los humanos.',
+        "fr": "Contrairement à Mickey Mouse, ne pas pouvoir utiliser le logiciel d'une personne décédée peut nuire aux humains.",
+        "it": 'Al  contrario di Topolino, non essere in grado di usare il software di una persona morta può danneggiare gli esseri umani.',
+        "lv": 'Atšķirībā no Mikija Peles, nespēja izmantot miruša cilvēka programmatūru var kaitēt cilvēkiem.',
+        "lt": 'Priešingai nei Peliukas Mikis, negalėjimas naudotis mirusio asmens programine įranga gali pakenkti žmonėms.',
+        "hu": 'Mickey egérrel ellentétben  az embereknek árthat, ha egy halott személy szoftverét nem használhatják.',
+        "nl": 'In tegenstelling tot Mickey Mouse, kan het niet kunnen gebruiken van software van een dode schadelijk zijn voor mensen.',
+        "pl": 'W przeciwieństwie do Myszki Miki, niemożność korzystania z oprogramowania od zmarłej osoby może zaszkodzić ludziom.',
+        "pt": 'Ao contrário do Rato Mickey, não poder  utilizar software de uma pessoa morta pode prejudicar os seres humanos.',
+        "ro": 'Spre deosebire de Mickey Mouse, faptul de a nu putea utiliza software-ul unei persoane moarte poate dăuna oamenilor.',
+        "sl": 'Za razliko od Miki Miške lahko to, da ne morete uporabljati programske opreme mrtve osebe, škoduje ljudem.',
+        "sk": 'Na rozdiel od Mickey Mouse, nemožnosť používať softvér od mŕtvej osoby môže ľuďom uškodiť.',
+        "fi": 'Toisin kuin Mikki Hiiri, kuolleen henkilön ohjelmistojen käyttämättömyys voi vahingoittaa ihmistä.',
+        "sv": 'Till skillnad från Musse Pigg kan det skada människor att inte kunna använda programvara från en död person.',
+        "cs": 'Na rozdíl od Mickey Mouse může nemožnost používat software od mrtvého člověka lidem uškodit.',
+        "el": 'Σε αντίθεση με τον Μίκυ Μάους, το να μην μπορείτε να χρησιμοποιήσετε το λογισμικό ενός νεκρού μπορεί να βλάψει τους ανθρώπους.',
+        "bg": 'За разлика от Мики Маус, невъзможността да се използва софтуер от мъртъв човек може да навреди на хората.'
     },
-    {
-        name: "Right to Repair",
-        description: "You should be able to repair broken electronic parts instead of having to buy a new device.",
-        image: "images/right-to-repair.png",
-        page: "/right-to-repair.html"
-    }
-];
+    "image": 'https://digital-justice.com/images/abandonware-continuity.png',
+    "page": 'abandonware-continuity.html'
+}, {
+    "name": 'Right to Repair',
+    "description": {
+        "da": 'Du bør kunne reparere ødelagte elektroniske dele i stedet for at skulle købe en ny enhed.',
+        "de": 'Man sollte kaputte elektronische Teile reparieren können, anstatt ein neues Gerät kaufen zu müssen.',
+        "et": 'Sul peaks olema võimalik remontida katkiseid elektroonilisi osi, selle asemel, et osta uus seade.',
+        "en": 'You should be able to repair broken electronic parts instead of having to buy a new device.',
+        "es": 'Debería poder reparar las piezas electrónicas rotas en lugar de tener que comprar un aparato nuevo.',
+        "fr": 'Vous devriez pouvoir réparer les pièces électroniques cassées au lieu de devoir acheter un nouvel appareil.',
+        "it": 'Dovresti essere in grado di riparare parti elettroniche rotte invece di dover comprare un nuovo dispositivo.',
+        "lv": 'Jums vajadzētu būt iespējai salūzušas elektroniskās detaļas salabot, nevis iegādāties jaunu ierīci.',
+        "lt": 'Turėtumėte galėti taisyti sugedusias elektronines dalis, užuot pirkę naują prietaisą.',
+        "hu": 'Ahelyett, hogy új készüléket kellene  vásárolnod, meg kellene tudnod javítani az elromlott elektronikai alkatrészeket.',
+        "nl": 'Je zou in staat moeten zijn om kapotte elektronische onderdelen te repareren in plaats van een nieuw apparaat te moeten kopen.',
+        "pl": 'Powinieneś być w stanie naprawić zepsute części elektroniczne zamiast kupować nowe urządzenie.',
+        "pt": 'Deverá ser capaz de reparar peças electrónicas partidas em vez de ter de comprar um dispositivo novo.',
+        "ro": 'Ar trebui să puteți repara piesele electronice stricate în loc să fiți nevoiți să cumpărați un dispozitiv nou.',
+        "sl": 'Pokvarjene elektronske dele bi morali imeti možnost popraviti, namesto da bi morali kupiti novo napravo.',
+        "sk": 'Mali by ste mať možnosť opraviť pokazené elektronické súčiastky namiesto toho, aby ste si museli kúpiť nové zariadenie.',
+        "fi": 'Rikkinäiset elektroniikan osat pitäisi pystyä korjaamaan sen sijaan, että joutuisi ostamaan uuden laitteen.',
+        "sv": 'Du borde kunna reparera trasiga elektroniska delar i stället för att behöva köpa en ny enhet.',
+        "cs": 'Měli byste mít možnost opravit rozbité elektronické součástky, místo abyste museli kupovat nové zařízení.',
+        "el": 'Θα πρέπει να μπορείτε να επισκευάζετε χαλασμένα ηλεκτρονικά εξαρτήματα αντί να χρειάζεται να αγοράσετε μια νέα συσκευή.',
+        "bg": 'Трябва да можете да поправяте счупени електронни части, вместо да се налага да купувате ново устройство.'
+    },
+    "image": 'https://digital-justice.com/images/right-to-repair.png',
+    "page": 'right-to-repair.html'
+}];
 
 const amountOfCards = 3;
 
@@ -42,7 +146,7 @@ function buildIdealCard(ideal) {
                 </div>
                 <div>
                     <h4>${ideal.name}</h4>
-                    <p>${ideal.description}</p>
+                    <p>${ideal.description[currentLanguage]}</p>
                     <a class="btn filled big round" href="${ideal.page}">More</a>
                 </div>
             </div>
